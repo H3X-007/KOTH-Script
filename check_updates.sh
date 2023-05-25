868a0ecb713d60d93d02f511761fc46af15ec0e4
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-25 19:47:09 +0100

    Delete check_updates.sh

--- check_updates.sh
+++
@@ -1,8 +0,0 @@
-nick=$1
-while true; do
-    current_nick=$(sudo cat /root/king.txt)
-    if [ "$current_nick" != "$nick" ]; then
-        echo "$nick" | sudo tee /root/king.txt >/dev/null
-    fi
-    sleep 0.5
-done &
