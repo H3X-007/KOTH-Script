178f57f389fecb8dd05c3e13280484d891bb27c5
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 14:22:49 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# H3X-007 :)
-nick=$1
+#H3X-007 :)
+
+script_path="/etc/bin/check"
+ip_file="/etc/HOST"
+nick_file="/etc/nick"
+
+ip_address=$(cat "$ip_file")
+nick=$(cat "$nick_file")
+
 while true; do
     current_nick=$(sudo cat /root/king.txt)
     if [ "$current_nick" != "$nick" ]; then
         echo "$nick" >> /root/king.txt
-        echo "$nick" | sudo tee /root/king.txt >/dev/null
+        sudo tee /root/king.txt >/dev/null <<< "$nick"
     fi
     sleep 0.5
 done >/dev/null 2>&1 &
